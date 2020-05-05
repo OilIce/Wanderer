689f410c4f15e55e99f7657cac76d77459f0b36f
OilIce <63580954+OilIce@users.noreply.github.com>
2020-05-05 15:54:44 +0300

    Update Еуые.py

--- Еуые.py
+++ Еуые.py
@@ -8,14 +8,23 @@ c.pack()
 plX= 400
 plY=300
 player = c.create_rectangle(plX, plY, plX+40, plY+60)
-def go(a,b):
-    c.move(player,a,b)
+vx = 1
+vy = 30
+def goX(x):
+    c.move(player, x, 0)
+a = 2
+def X(d):
+    vy = d
+    return vy
+def goY():
+    global a, vy, player
+    vy = vy - a
+    c.move(player, 0, -vy)
+    if c.coords(player)[3] < 440:
+        w.after(30, goY)
 pass
-i = 15
-def jump(a,b):
-    i-=1
-    c.move(player,a,b*i)
-w.bind("<Left>", lambda event: go(-10,0))
-w.bind("<Right>", lambda event: go(10,0))
-w.bind("<Up>", lambda event: jump(0,1))
+w.bind("<Left>", lambda event: goX(-10))
+w.bind("<Right>", lambda event: goX(10))
+w.bind("<Up>", lambda event: X(30))
+goY()
 w.mainloop()
