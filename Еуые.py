950098c8c48244f1bfcb189647878065e585f153
OilIce <63580954+OilIce@users.noreply.github.com>
2020-05-11 17:43:09 +0300

    Update Еуые.py

--- Еуые.py
+++ Еуые.py
@@ -1,35 +1,48 @@
 import tkinter as tk
+from tkinter import NW
+
 w = tk.Tk()
 w.geometry("960x540")
 w.resizable(False, False)
-w.title("ЗапускайсяСволочь")
+w.title("Wanderer")
 c = tk.Canvas(w, width=960, height=540, bg="white")
 c.pack()
-plX= 400
-plY=300
-player = c.create_rectangle(plX, plY, plX+40, plY+60)
+image = tk.PhotoImage(file = "C:/Users/Admin/Desktop/ЫЫыыЫСаИ/pervayakomnata.png")
+c.create_image(0, 0, image = image, anchor = NW)
+plX = 400
+plY = 300
+player = c.create_rectangle(plX, plY, plX+40, plY+60, fill = "green")
 vx = 1
 vy = 30
-def goX(x):
-    c.move(player, x, 0)
+def goX2(x):
+    if c.coords(player)[0] > 60:
+        c.move(player, x, 0)
+def goX1(x):
+    if c.coords(player)[2] < 900:
+        c.move(player, x, 0)
 a = 2
 def X(d):
     global vy
     vy = d
-    if c.coords(player)[3] >= 440:
+    if c.coords(player)[3] >= 463:
         c.move(player, 0, -vy)
         goY()
 def goY():
     global a, vy, player
     vy = vy - a
     c.move(player, 0, -vy)
-    if c.coords(player)[3] < 440:
+    if c.coords(player)[1] < 75:
+        vy = 0
         w.after(30, goY)
+    elif c.coords(player)[3] < 463:
+        w.after(30, goY)
+    elif c.coords(player)[3] == 310 and 100 < c.coords(player)[0] < 200:
+        vy = 0
+        w.bind("<Up>", lambda event: X(30))
     else:
         w.bind("<Up>", lambda event: X(30))
 pass
-w.bind("<Left>", lambda event: goX(-10))
-w.bind("<Right>", lambda event: goX(10))
-w.bind("<Up>", lambda event: X(30))
 goY()
+w.bind("<Left>", lambda event: goX2(-10))
+w.bind("<Right>", lambda event: goX1(10))
 w.mainloop()
